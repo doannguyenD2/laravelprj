3738b57ef33987ef4cae852fe70c3a85c1432020
doannguyenD2 <40143494+doannguyenD2@users.noreply.github.com>
2020-01-04 23:36:17 +0700

    add note

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -27,4 +27,9 @@ class AuthController extends Controller
         $accessToken = $user->createToken('authToken')->accessToken;
         return response(['user'=> $user, 'accessToken' => $accessToken]);
     }
+
+    public function login(Request $request)
+    {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('register', 'Api\AuthController@register');
+Route::post('login','Api\AuthController@login');
